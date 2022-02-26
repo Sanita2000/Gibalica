3b7d4fd99c8998beaeb8cfda486c79c4e202a048
Karalyos Anita <anita.karalyos@ekozig.hu>
2022-02-26 10:59:39 +0100

    base, gradle

---
+++ app/src/main/java/com/example/gibalica/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.gibalica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.mlkit.vision.pose.accurate.AccuratePoseDetectorOptions;
+import com.google.mlkit.vision.pose.defaults.PoseDetectorOptions;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
