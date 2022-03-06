0b0f9ffa0a177de25c64d18aa1d35b3759b4eb6b
Sentashi <ivansentic99@gmail.com>
2022-03-07 00:28:58 +0100

    Settings buttons listeners

--- app/src/main/java/com/example/gibalica/SettingsActivity.java
+++ app/src/main/java/com/example/gibalica/SettingsActivity.java
@@ -1,11 +1,16 @@
 package com.example.gibalica;
 
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
 public class SettingsActivity extends AppCompatActivity {
     SharedPrefs sp;
+    Switch tts, nightMode, contrast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -14,8 +19,21 @@ public class SettingsActivity extends AppCompatActivity {
         // use sp to change settings values
         sp  = new SharedPrefs(this);
 
+        tts = (Switch) findViewById(R.id.switch1);
+        nightMode = (Switch) findViewById(R.id.switch2);
+        contrast = (Switch) findViewById(R.id.switch3);
 
-
+        tts.setOnCheckedChangeListener(listener);
+        nightMode.setOnCheckedChangeListener(listener);
+        contrast.setOnCheckedChangeListener(listener);
     }
 
+    CompoundButton.OnCheckedChangeListener listener = (buttonView, isChecked) -> {
+        if(buttonView.isChecked()){
+            Toast.makeText(SettingsActivity.this, buttonView.getText() , Toast.LENGTH_SHORT).show();
+        }else{
+
+        }
+
+    };
 }
